6bf72f9116c3de980cfa9a0a2900a1096867a344
Romanarcekessler <romanarce16@gmail.com>
2024-07-10 13:35:56 -0300

    Update sender bbb

--- index.js
+++ index.js
@@ -28,14 +28,14 @@ app.post('/send-message', async (req, res) => {
   try {
     const response = await client.messages.create({
       contentSid: 'HX9b638f2528bb6a26939ccbe2d6ccf6ca',
-      from: 'whatsapp:+15304530886',
+      from: '+15304530886',
       contentVariables: JSON.stringify({
         1: username,
         2: query,
         3: phone
       }),
       messagingServiceSid: 'MG697fa907221a26b2da9cbc99068577b1',
-      to: 'whatsapp:+5493564522800'
+      to: '+5493564522800'
     });
 
     if (response.sid) {
