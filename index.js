56def87a82c2aa1798f7438d93d07a91c8d22fcb
Romanarcekessler <romanarce16@gmail.com>
2024-07-12 14:09:09 -0300

    update precio

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ app.post('/parse-json-response', (req, res) => {
     console.log('Body: ', jsonBody);
     
     let responseString = jsonBody.map((item, index) => {
-      return `${index + 1}). Tipo de propiedad: ${item['0']}, Dirección: ${item['1']}, Dormitorios: ${item['2']}, Garage: ${item['3']}, Servicios: ${item['4']}, Mascotas: ${item['5']}`;
+      return `${index + 1}). Tipo de propiedad: ${item['0']}, Dirección: ${item['1']}, Dormitorios: ${item['2']}, Garage: ${item['3']}, Servicios: ${item['4']}, Mascotas: ${item['5']}, Precio: ${item['7']}`;
     }).join('\n');
 
     res.status(200).send(responseString);
