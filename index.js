9dae4ae126628b13f9665e7515e6921ce0817317
Romanarcekessler <romanarce16@gmail.com>
2024-09-11 11:34:41 -0300

    update history urls size

--- index.js
+++ index.js
@@ -514,8 +514,8 @@ app.post('/get-itinerary-url', async (req, res) => {
       conversationHistory = [];
     }
 
-    if (conversationHistory.length > 4) {
-      conversationHistory = conversationHistory.slice(-4);
+    if (conversationHistory.length > 2) {
+      conversationHistory = conversationHistory.slice(-2);
     }
     console.log('Conversation HISTORY: ', conversationHistory);
 
