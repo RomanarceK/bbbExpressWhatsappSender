7d973613a15cb96b0490e8c476123a57f1acd0ca
Romanarcekessler <romanarce16@gmail.com>
2024-09-16 14:15:38 -0300

    Update config mongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_DB_URI;
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,  // Asegura compatibilidad
+  ssl: true,  // Activa SSL
+});
 
 async function connectToDatabase() {
   try {
