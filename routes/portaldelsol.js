702b174f2a990be19d66a764d9710b4d403095e1
Romanarcekessler <romanarce16@gmail.com>
2025-04-22 20:56:09 -0300

    Add notify urls portaldelsol

--- routes/portaldelsol.js
+++ routes/portaldelsol.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const router = Router();
 const { getConversationNewUI, saveConversationNewUI } = require('../hooks/useConversations');
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const axios = require('axios');
 
 router.post('/ask', async (req, res) => {
@@ -58,6 +59,65 @@ router.post('/ask', async (req, res) => {
   } catch (error) {
     console.error('Error al procesar con ChatGPT:', error);
     res.status(500).json({ success: false, error: 'Error al procesar con ChatGPT' });
+  }
+});
+
+router.post('/terrenos-notify', async (req, res) => {
+  const { zona, username, phone, payment } = req.body;
+
+  try {
+    const response = await client.messages.create({
+      contentSid: 'HX3eef5d7b07de6de52f8cfbea7b4f317c',
+      from: 'whatsapp:+17074021487',
+      contentVariables: JSON.stringify({
+        1: username,
+        2: zona,
+        3: payment,
+        4: phone,
+      }),
+      messagingServiceSid: 'MG697fa907221a26b2da9cbc99068577b1',
+      to: 'whatsapp:+5493564339696'
+    });
+
+    if (response.sid) {
+      console.log(`Mensaje enviado a WhatsApp: ${phone}`);
+      res.status(200).json({success: true, message: 'Mensaje enviado a WhatsApp'});
+    } else {
+      console.error('Error al enviar mensaje, respuesta sin SID.');
+      res.status(500).send('Error al enviar mensaje');
+    }
+  } catch (error) {
+    console.error(`Error al enviar mensaje: ${error.message}`);
+    res.status(500).send(`Error al enviar mensaje: ${error.message}`);
+  }
+});
+
+router.post('/casas-notify', async (req, res) => {
+  const { username, phone, payment } = req.body;
+
+  try {
+    const response = await client.messages.create({
+      contentSid: 'HX75e67318b0ccf7f79a5151cb359c9fa5',
+      from: 'whatsapp:+17074021487',
+      contentVariables: JSON.stringify({
+        1: username,
+        2: payment,
+        3: phone,
+      }),
+      messagingServiceSid: 'MG697fa907221a26b2da9cbc99068577b1',
+      to: 'whatsapp:+5493564339696'
+    });
+
+    if (response.sid) {
+      console.log(`Mensaje enviado a WhatsApp: ${phone}`);
+      res.status(200).json({success: true, message: 'Mensaje enviado a WhatsApp'});
+    } else {
+      console.error('Error al enviar mensaje, respuesta sin SID.');
+      res.status(500).send('Error al enviar mensaje');
+    }
+  } catch (error) {
+    console.error(`Error al enviar mensaje: ${error.message}`);
+    res.status(500).send(`Error al enviar mensaje: ${error.message}`);
   }
 });
 
